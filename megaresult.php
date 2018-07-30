d1f78cee0dfa47937d3204c9cf5f2b84f1fb7935
Paul Williams <paul@highaltitudearchery.com>
2018-07-29 22:38:20 -0600

    testing how shortcode attribs work

--- megaresult.php
+++ megaresult.php
@@ -2,14 +2,14 @@
 /*
 Plugin Name: MegaResult Sports Result Display
 Plugin URI: tbd
-Version: 1.0.0
+Version: 1.0.1
 Author: Paul Williams
 Author URI: tbd
 */
 
 
 function mr_megaresult_shortcode_handler( $attrs, $content=null) {
-  return "look, a shortcode!"
+  return "look, a shortcode! has attribs: " + print_r($attrs,true);
 }
 
 add_shortcode( 'megaresult', 'mr_megaresult_shortcode_handler' );
