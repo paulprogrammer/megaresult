028f05906b4db823c88c8c603229b73232f0d655
Paul Williams <williamspaul@google.com>
2018-08-01 16:35:52 -0600

    refining admin page

--- megaresult.php
+++ megaresult.php
@@ -8,15 +8,17 @@ Author URI: tbd
 */
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
+global $wpdb;
+
 function mr_megaresult_shortcode_handler( $attrs, $content=null) {
   return "look, a shortcode!";
 }
 add_shortcode( 'megaresult', 'mr_megaresult_shortcode_handler' );
 
 function mr_megaresult_admin_menu() {
-  add_menu_page("MegaResult", "MegaResult", "manage_options", "megaresult-plugin", "mr_megaresult_admin_init");
+  add_menu_page("MegaResult", "MegaResult", "manage_options", "megaresult-plugin/megaresult-admin.php", "mr_megaresult_admin_init");
 }
 function mr_megaresult_admin_init() {
-  echo "test";
+
 }
 add_action("admin_menu", "mr_megaresult_admin_menu");
